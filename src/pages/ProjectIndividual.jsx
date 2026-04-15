5f5fd5887644bdd1b68a6f384b0dee9d38cbb55e
Annie Howells <anniehowells@gmail.com>
2026-04-15 11:32:27 -0400

    spacing issue

--- src/pages/ProjectIndividual.jsx
+++ src/pages/ProjectIndividual.jsx
@@ -76,14 +76,12 @@ function VideoFull({ video }) {
   if (isVimeo) {
     const id = video.match(/vimeo\.com\/(\d+)/)?.[1]
     return (
-      <div style={{ position: 'relative', paddingTop: '56.25%', width: '100%' }}>
-        <iframe
-          src={`https://player.vimeo.com/video/${id}?autoplay=1&loop=1&muted=1&background=1`}
-          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'none' }}
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
+      <iframe
+        src={`https://player.vimeo.com/video/${id}?autoplay=1&loop=1&muted=1&background=1`}
+        style={{ width: '100%', aspectRatio: '16 / 9', border: 'none', display: 'block' }}
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+      />
     )
   }
   return (
